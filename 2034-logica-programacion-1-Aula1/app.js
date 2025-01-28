d1c454487c16babee3b5cc77640581d05d6eb142
eljijijaja <pedrochonay555@gmail.com>
2025-01-27 18:15:36 -0600

    Implement number guessing game with limited attempts and hints

--- 2034-logica-programacion-1-Aula1/app.js
+++ 2034-logica-programacion-1-Aula1/app.js
@@ -1,10 +1,32 @@
-let numeroSecreto = 6;
-let numeroUsuario = prompt("!Adivina el número que estoy pensando¡:");
 
-console.log(numeroUsuario);
+let limite = 100;
+let numeroSecreto = Math.floor(Math.random() * limite) + 1;
+let numeroUsuario = 0;
+let intentos = 1;
+let maximosIntentos = 4;
 
-if (numeroUsuario == numeroSecreto) {
-    alert('Acertaste el numero');
-} else {
-    alert('No acertaste el numero');
+console.log(numeroSecreto);
+
+while (numeroSecreto != numeroUsuario) { 
+    numeroUsuario = parseInt(prompt(`!Adivina el número que estoy pensando del 1 al ${limite}!`));
+
+    console.log(typeof(numeroUsuario));
+
+    // codigo de comparacion 
+    if (numeroUsuario == numeroSecreto) {   
+            alert(`Acertaste, el numero es: ${numeroUsuario}, lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    } else {
+        if (numeroUsuario > numeroSecreto) {
+                alert('El número es menor');
+        } else {
+                alert('El número es mayor');
+        }
+        //incremento del contador cuando no acierta
+        intentos++;
+        if (intentos > maximosIntentos) {
+            alert(`Se acabaron tus ${maximosIntentos} intentos`);
+            break;    
+        }
+            }
 }
+
