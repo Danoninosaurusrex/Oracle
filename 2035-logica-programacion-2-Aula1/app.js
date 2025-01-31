4c533e96428c2c625c246c793e35edc2b3d0f357
eljijijaja <pedrochonay555@gmail.com>
2025-01-30 19:26:32 -0600

    proyectro numero 2 del plan Oracle

---
+++ 2035-logica-programacion-2-Aula1/app.js
@@ -0,0 +1,10 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'Juego del numero secreto';
+
+let parafo = document.querySelector('p');
+parafo.innerHTML = 'Indica un numero del 1 al 10';
+
+
+function intentoDeUsuario (){
+    alert ('click en el boton desde js');
+};
